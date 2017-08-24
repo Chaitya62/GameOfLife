0a23d4c08fc008d17b4e683fadb0323c4a5ce6e2
Chaitya Shah <Chaitya62@gmail.com>
2017-08-24 16:40:44 +0530

    Add fill square

--- GAME.C
+++ GAME.C
@@ -6,21 +6,21 @@
 #include<dos.h>
 #define HEIGHT 480
 #define WIDTH 640
+#define BOX_SIZE 10
+#define PADDING 2*BOX_SIZE
+
 union REGS in,out;
 int game_start = 0;
+short game_board[1000][1000];
+
+/* MOUSE FUNCTIONS */
 
 int callmouse(){
 	in.x.ax=1;
 	int86(51,&in,&out);
 	return 1;
 }
-void mouseposi(int *xpos,int *ypos,int *click){
-	in.x.ax=3;
-	int86(51,&in,&out);
-	*click=out.x.bx;
-	*xpos=out.x.cx;
-	*ypos=out.x.dx;
-}
+
 int mousehide()
 {
   in.x.ax=2;
@@ -35,19 +35,63 @@ void setposi(int *xpos,int *ypos)
    int86(51,&in,&out);
 }
 
-int draw_matrix(int box_size){
-  int x_lines,y_lines,i,padding;
-  padding = 2*box_size;
+void mouseposi(int *xpos,int *ypos,int *click){
+  in.x.ax=3;
+  int86(51,&in,&out);
+  *click=out.x.bx;
+  *xpos=out.x.cx;
+  *ypos=out.x.dx;
+}
+
+
+
+/* DRAW AND FILL FUNCTIONS */
+
+int drawfillsquare(int x, int y, int size,int color){
+    int i;
+    setcolor(color);
+    for(i = y;i<y+size;i++){
+	line(x, i, x+size, i);
+    }
+    return 0;
+}
+
+int fill_box(int mousex, int mousey){
+  int x = (mousex/BOX_SIZE)*BOX_SIZE;
+  int y = (mousey/BOX_SIZE)*BOX_SIZE;
+
+  // if outside boundary
+  if(x >= WIDTH-PADDING || x < PADDING || y >= HEIGHT-PADDING || y < PADDING ) {
+      return -1;
+  }
+  game_board[x][y] = !game_board[x][y];
+  mousehide();
+  //setfillstyle(SOLID_FILL, RED);
+  drawfillsquare(x+1, y+1, BOX_SIZE-2, RED);
+  //rectangle(x, y, x + BOX_SIZE, y + BOX_SIZE);
+  //floodfill(x+BOX_SIZE/2, y + BOX_SIZE/2, RED);
+  callmouse();
+  return 0;
+}
+
+
+int draw_game_board(int box_size){
+  int x_lines,y_lines,i;
   x_lines = WIDTH/box_size -  4;
   y_lines = HEIGHT/box_size - 4;
-  for(i = padding; i<WIDTH-padding; i+=box_size){
-    line(i, padding, i, HEIGHT-padding);
+  setcolor(WHITE);
+  for(i = PADDING; i<=WIDTH-PADDING; i+=box_size){
+    line(i, PADDING, i, HEIGHT-PADDING);
   }
-  for(i = padding; i<HEIGHT-padding; i+=box_size){
-    line(padding, i, WIDTH-padding, i);
+  for(i = PADDING; i<=HEIGHT-PADDING; i+=box_size){
+    line(PADDING, i, WIDTH-PADDING, i);
   }
   return 0;
 }
+
+
+
+/*  GAME LOOP */
 
 int gameloop(){
   int x,y, cl;
@@ -65,15 +109,16 @@ int gameloop(){
       game_start = 1;
       cleardevice();
       mousehide();
-      draw_matrix(4);
+      draw_game_board(BOX_SIZE);
       callmouse();
-      
+
     }
 
     }else{
-      
-
+       if(cl == 1)  fill_box(x, y);
     }
+
+    delay(60);
    }while(!kbhit());
    return 0;
 }
